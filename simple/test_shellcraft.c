a6615514d416fe02a4f2ea54dcb553e0c0bd07d8
Johnny Mkhael <johnny.mkhael@gmail.com>
2021-11-02 17:43:04 +0100

    adding simple bof example

---
+++ simple/test_shellcraft.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <unistd.h> 
+
+void start(){ 
+char input[500];
+gets(input);
+}
+
+int main(){ 
+printf("Hello There. Do you have an input for me?\n");
+start();
+}
